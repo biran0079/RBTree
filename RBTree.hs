9a4f2935a70af1c41d1da88da35bd4562c346a8c
Ran Bi <bir@fb.com>
2019-08-11 22:33:20 -0700

    format change

--- RBTree.hs
+++ RBTree.hs
@@ -34,10 +34,12 @@ isBlack (Node Red _ _ _) = False
 isBlack _ = True
 
 balL :: Color -> a -> (RBTree a, Bool) -> RBTree a -> (RBTree a, Bool)
-balL color y (left, balanced) right = if balanced then (Node color y left right, True) else balL' color y left right
+balL color y (left, True) right = (Node color y left right, True)
+balL color y (left, False) right = balL' color y left right
 
 balR :: Color -> a -> RBTree a -> (RBTree a, Bool) -> (RBTree a, Bool)
-balR color y left (right, balanced) = if balanced then (Node color y left right, True) else balR' color y left right
+balR color y left (right, True) = (Node color y left right, True)
+balR color y left (right, False) = balR' color y left right
 
 balL' :: Color -> a -> RBTree a -> RBTree a -> (RBTree a, Bool)
 balL' color1 p n (Node color2 s sl sr) 
@@ -55,16 +57,15 @@ balR' color1 p (Node color2 s sl sr) n
 
 delete :: (Ord a) => a -> RBTree a -> RBTree a 
 delete x t = fst $ delete' x t
-  where
-    delete' x Nil = (Nil, True)
-    delete' x t@(Node color y left right)
-        | x < y = balL color y (delete' x left) right
-        | x > y = balR color y left (delete' x right)
-        | otherwise = deleteRoot t
-    deleteRoot (Node color _ Nil Nil) = (Nil, color == Red)
-    deleteRoot (Node _ _ left Nil) = (blacken left, True) 
-    deleteRoot (Node _ _ Nil right) = (blacken right, True)
-    deleteRoot (Node color _ left right) = let m = findMin right in balR color m left (delete' m right)
-    findMin (Node _ x Nil _) = x
-    findMin (Node _ _ left _) = findMin left
+  where delete' x Nil = (Nil, True)
+        delete' x root@(Node color y left right)
+            | x < y = balL color y (delete' x left) right
+            | x > y = balR color y left (delete' x right)
+            | otherwise = deleteRoot root
+        deleteRoot (Node color _ Nil Nil) = (Nil, color == Red)
+        deleteRoot (Node _ _ left Nil) = (blacken left, True) 
+        deleteRoot (Node _ _ Nil right) = (blacken right, True)
+        deleteRoot (Node color _ left right) = let m = findMin right in balR color m left (delete' m right)
+        findMin (Node _ x Nil _) = x
+        findMin (Node _ _ left _) = findMin left
 
